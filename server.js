280206f046883d8c1ab4ed3d56c90e7f514d25ef
dwetoskey <dwetoskey224@gmail.com>
2022-03-04 14:03:20 -0500

    worked on server

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
+const notes = require("./db/db.json");
+
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.route("/api/notes")
+  .get
+
+
+
+app.get("/public/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.listen(PORT, () => {
+  console.log("App running on PORT " + PORT);
+});
